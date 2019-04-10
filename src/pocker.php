46c27f2d98421d86499e9d3d8f434bc97dea8fc0
hanzefang <hanzefang@gmail.com>
2019-04-10 20:13:59 +0800

    init submit

---
+++ src/pocker.php
@@ -0,0 +1,30 @@
+<?php
+require '../vendor/autoload.php';
+
+class Pocker{
+    private static $instance = null;
+    private $guzzleClient = null;
+
+    private $host = 'localhost';
+    private $port = '4201';
+    private $apiVersion = 'v1.39';
+
+    private function __construct(){
+        $this->guzzleClient = new GuzzleHttp\Client();
+    }
+
+    public static function getInstance(){
+        if(self::$instance == null){
+            self::$instance = new Pocker();
+        }
+        
+        return self::$instance;
+    }
+
+    public function getInfo(){
+        $res = $this->guzzleClient->request('GET', 'http://192.168.111.129:4201/info');
+        echo $res->getStatusCode();
+        echo $res->getHeader('content-type')[0];
+        echo $res->getBody();
+    }
+}
